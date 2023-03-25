497cc94717361b393af08d8710e75ce8bbf14045
quangdethuong <quangnn1106@gmail.com>
2023-03-25 21:19:21 +0700

    fix sumdigit

--- JAVACORE_SUMDIGIT/src/Main.java
+++ JAVACORE_SUMDIGIT/src/Main.java
@@ -1,24 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        System.out.print("Enter number: ");
-//        int num = Integer.parseInt(sc.nextLine());
-//        int sum = 0;
-//
-//        while (num > 0){
-//            sum += num % 10;
-//            num /= 10;
-//        }
-//
-//        System.out.print("Result: " + sum);
-//    }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int tuoi = scanner.nextInt();
-        String ten = scanner.nextLine();
-        System.out.println(ten + tuoi);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int num = Integer.parseInt(sc.nextLine());
+        int sum = 0;
+
+        while (num > 0){
+            sum += num % 10;
+            num /= 10;
+        }
+
+        System.out.print("Result: " + sum);
     }
+
+
 }
